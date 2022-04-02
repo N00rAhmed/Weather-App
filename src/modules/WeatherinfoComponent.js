eda812a850285d5db3f9104774392c9d4f56e66d
unknown <tronn232003@gmail.com>
2022-04-02 19:07:15 +0100

    First commit

--- src/modules/WeatherinfoComponent.js
+++ src/modules/WeatherinfoComponent.js
@@ -41,25 +41,25 @@ align-items: center;
 flex-wrap: wrap;
 `;
 const InfoContainer = styled.div`
-  display: flex;
-  margin: 5px 10px;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
+display: flex;
+margin: 5px 10px;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
 `;
 const InfoIcon = styled.img`
   width: 36px;
   height: 36px;
 `;
 const InfoLabel = styled.span`
-  display: flex;
-  flex-direction: column;
-  font-size: 14px;
-  margin: 15px;
-  & span {
-    font-size: 12px;
-    text-transform: capitalize;
-  }
+display: flex;
+flex-direction: column;
+font-size: 14px;
+margin: 15px;
+& span {
+  font-size: 12px;
+  text-transform: capitalize;
+}
 `;
 const WeatherInfoComponent=()=>{
     return(
@@ -73,9 +73,12 @@ const WeatherInfoComponent=()=>{
 }
 
 const WeatherComponent =()=>{
-    return <>
+    return (
+    <>
     <WeatherCondition>
-        <Condition><span>30 C</span> | Cloudy</Condition>
+        <Condition>
+          <span>30 C</span> | Cloudy
+        </Condition>
         <WeatherLogo src='/icons/perfect-day.svg'/>
     </WeatherCondition>
     <Location>London, GB</Location>
@@ -86,6 +89,7 @@ const WeatherComponent =()=>{
         <WeatherComponent/>
         <WeatherComponent/>
     </WeatherInfoContainer>
-    </>;
+    </>
+    );
 };
 export default WeatherComponent;

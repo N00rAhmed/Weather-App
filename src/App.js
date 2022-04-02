68cf7fa3c44b907fb6aecb63276e2f36425223e0
Muntasir <muntasirenamalhuda@gmail.com>
2022-04-03 00:01:09 +0100

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import CityComponent from './modules/CityComponent';
 import WeatherComponent from './modules/WeatherinfoComponent';
 
-
 const Container = styled.div`
-display:flex;
-flex-direction:column;
-margin: auto;
-align-items: center;
-box-shadow: 0 3px 6px 0 #555;
-padding: 20px 10px;
-border-radius: 4px;
-width: 380px;
-background: white;
-font-family: Montserrat;
+	display: flex;
+	flex-direction: column;
+	margin: auto;
+	align-items: center;
+	box-shadow: 0 3px 6px 0 #555;
+	padding: 20px 10px;
+	border-radius: 4px;
+	width: 380px;
+	background: white;
+	font-family: Montserrat;
 `;
 
 const AppLabel = styled.span`
-color:black;
-font-size: 18px;
-font-weight: bold;
+	color: black;
+	font-size: 18px;
+	font-weight: bold;
 `;
 
 function App() {
-  return (
-  <Container>
-    <AppLabel>React Weather Application</AppLabel>
-    <WeatherComponent />
-    </Container>
-  );
+	return (
+		<Container>
+			<AppLabel>React Weather Application</AppLabel>
+			<WeatherComponent />
+		</Container>
+	);
 }
 
 export default App;

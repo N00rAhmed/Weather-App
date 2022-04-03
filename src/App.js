bd7146a592e1a12c76542354c7d2398f922c83e4
unknown <tronn232003@gmail.com>
2022-04-03 06:04:33 +0100

    fixed all the bugs now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import { useState } from 'react';
-import styled from 'styled-components'
-import CityComponent from './modules/CityComponent';
-import WeatherComponent from './modules/WeatherinfoComponent';
+import React, { useState } from "react";
+import styled from "styled-components";
 import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherinfoComponent";
 
+export const WeatherIcons = {
+  "01d": "/icons/sunny.svg",
+  "01n": "/icons/night.svg",
+  "02d": "/icons/day.svg",
+  "02n": "/icons/cloudy-night.svg",
+  "03d": "/icons/cloudy.svg",
+  "03n": "/icons/cloudy.svg",
+  "04d": "/icons/perfect-day.svg",
+  "04n": "/icons/cloudy-night.svg",
+  "09d": "/icons/rain.svg",
+  "09n": "/icons/rain-night.svg",
+  "10d": "/icons/rain.svg",
+  "10n": "/icons/rain-night.svg",
+  "11d": "/icons/storm.svg",
+  "11n": "/icons/storm.svg",
+};
 
-// const API_KEY = 'f2f2db00d9b78c3c5474b16236f733d9'
 const Container = styled.div`
-display:flex;
-flex-direction:column;
-margin: auto;
-align-items: center;
-box-shadow: 0 3px 6px 0 #555;
-padding: 20px 10px;
-border-radius: 4px;
-width: 380px;
-background: white;
-font-family: Montserrat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
 `;
 
 const AppLabel = styled.span`
-color:black;
-font-size: 18px;
-font-weight: bold;
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CloseButton = styled.span`
+  padding: 2px 3px;
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
 `;
 
 function App() {
@@ -31,14 +54,13 @@ function App() {
   const fetchWeather = async (e) => {
     e.preventDefault();
     const response = await Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2f2db00d9b78c3c5474b16236f733d9`,
-      );
-      updateWeather(response.data);
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+    );
+    updateWeather(response.data);
   };
-  
   return (
-  <Container>
-      <AppLabel>React Weather App</AppLabel>
+    <Container>
+      <AppLabel>React Weather Application</AppLabel>
       {city && weather ? (
         <WeatherComponent weather={weather} city={city} />
       ) : (
